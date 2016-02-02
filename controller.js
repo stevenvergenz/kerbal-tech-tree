c1890762a0756e031b67cb8d79f456e565aa95e5
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2016-02-02 09:06:48 -0500

    Finished pan/zoom

--- controller.js
+++ controller.js
@@ -16,25 +16,51 @@ app.controller('KerbalTechController', ['$scope', '$http', function($scope,$http
 	};
 }]);
 
-app.directive('dgyZoomPan', function(){
+app.directive('kspZoomPan', function(){
 	return {
 		link: function($scope, elem, attrs)
 		{
+			elem = elem[0];
 			var zoom = 100;
 
-			var style = window.getComputedStyle(elem[0]);
-			var width = parseInt(style.width), height = parseInt(style.height);
-			var center = [elem[0].scrollLeft+width/2, elem[0].scrollTop+height/2];
+			elem.scrollLeft = 850;
+			elem.scrollTop = 500;
 
-			elem[0].addEventListener('wheel', function(evt)
+			elem.addEventListener('wheel', function(evt)
 			{
 				evt.preventDefault();
+
+				var center = [elem.scrollLeft+elem.clientWidth/2, elem.scrollTop+elem.clientHeight/2];
 				zoom = Math.max(40, Math.min(200, evt.deltaY > 0 ? zoom-5 : zoom+5));
-				elem[0].style.zoom = zoom + '%';
-				elem[0].scrollLeft = center[0] - (zoom/100)*width/2;
-				elem[0].scrollTop = center[1] - (zoom/100)*height/2;
-				console.log(zoom, center, elem[0].scrollLeft, elem[0].scrollTop);
+
+				elem.style.zoom = zoom + '%';
+				elem.scrollLeft = center[0] - elem.clientWidth/2;
+				elem.scrollTop = center[1] - elem.clientHeight/2;
 			});
+
+
+			var dragStart = null;
+			elem.addEventListener('mousedown', function(evt){
+				dragStart = {
+					x: evt.clientX,
+					y: evt.clientY
+				};
+			});
+
+			elem.addEventListener('mouseup', function(evt){
+				dragStart = null;
+			});
+
+			elem.addEventListener('mousemove', function(evt){
+				if(dragStart){
+					elem.scrollLeft -= (100/zoom)*(evt.clientX-dragStart.x);
+					elem.scrollTop -= (100/zoom)*(evt.clientY-dragStart.y);
+					dragStart.x = evt.clientX;
+					dragStart.y = evt.clientY;
+				}
+			});
+
+
 		}
 	}
 });
